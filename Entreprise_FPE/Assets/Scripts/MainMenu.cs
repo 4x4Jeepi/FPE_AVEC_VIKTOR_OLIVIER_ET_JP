d209f21762439c4bdc85d05f97c5d742779f098d
4x4jeepi <jphilippe2705@gmail.com>
2018-07-06 15:06:24 -0400

    JP-Maine Menu fonctionnel

--- Entreprise_FPE/Assets/Scripts/MainMenu.cs
+++ Entreprise_FPE/Assets/Scripts/MainMenu.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour 
 {
@@ -10,14 +11,14 @@ public class MainMenu : MonoBehaviour
 
 	public GameObject m_StartMenu;
 	public GameObject m_SelectLvlMenu;
-	public GameObject m_Player1Select;
-	public GameObject m_Player2Select;
+	public GameObject m_MenuPlayer1Select;
+	public GameObject m_MenuPlayer2Select;
 
 	public GameObject[] m_ShipPrefabs;
-	public Vector3[] m_Position;
+	public Transform[] m_Position;
 
 	private bool m_Lvl2Active = false;
-	private bool m_2player;
+	private bool m_2player = false;
 	private string m_Scenes;
 
 	public void GoToSelectLvl()
@@ -29,5 +30,32 @@ public class MainMenu : MonoBehaviour
 	public void SetScenes(string a_ScenesName)
 	{
 		m_Scenes = a_ScenesName;
+        m_SelectLvlMenu.SetActive(false);
+        m_MenuPlayer1Select.SetActive(true);
 	}
+
+    public void SetSpaceShipWanted(int a_Choice)
+    {
+        if (!m_2player)
+        {
+            GameObject a_Player = Instantiate(m_ShipPrefabs[a_Choice], m_Position[a_Choice]);
+            DontDestroyOnLoad(a_Player);
+            m_MenuPlayer1Select.SetActive(true);
+
+            SceneManager.LoadScene(m_Scenes);
+        }
+        else
+        {
+            GameObject a_Player = Instantiate(m_ShipPrefabs[a_Choice], m_Position[a_Choice]);
+            DontDestroyOnLoad(a_Player);
+            m_MenuPlayer1Select.SetActive(false);
+            m_MenuPlayer2Select.SetActive(true);
+            m_2player = !m_2player;
+        }
+    }
+
+    public void Set2Player()
+    {
+        m_2player = !m_2player;
+    }
 }
